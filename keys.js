af3baca8b610ae0d4f5fb1fa1a13d36c212b1e17
Yorali <hideayeni@gmail.com>
2017-07-21 22:36:14 -0500

    Complete

--- keys.js
+++ keys.js
@@ -1,5 +1,14 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  
+  consumer_key: 'koO8tQWVQqbn4K4vKs3H7arSJ',
+  consumer_secret: '6kzMOUrnhnTSDEjasZAg32VUAi77LA2Siyw7EfNEDxQ1lJp477',
+  access_token_key: '886674993265987584-d0HE904kEJQz1LpxmjWBCdZl0Fuv1JW',
+  access_token_secret: 'bh0vzikje71bNQrmIQ8mwIvYAZZ3K4DNxm9OgtxtpHnqa',
+}
+
+exports.spotifyKeys = {
+  id: '0d750107bc4d42a8b4009f2f6ea53852',
+  secret: '2fc2a37ef7334f31ac5a493b2686e4a0'
+
 }
--- liri.js
+++ liri.js
@@ -1,3 +1,136 @@
 var box = require('./keys.js');
+var request = require('request');
+var filez = require('file-system');
 
-var keys = box.twitterKeys,
+var jax = require('ajax-request');
+
+liri = function (command, qry) {
+
+    command = process.argv[2] || command;
+    qry = process.argv[3] || qry;
+
+    if (command === 'my-tweets') {
+        //pull last 20 tweets
+        var Twitter = require('twitter');
+
+        var keys = new Twitter(box.twitterKeys);
+
+        var params = {screen_name: 'IzayoiLiyen', count: 20}
+        keys.get('statuses/user_timeline', params, function(err, tweets, response) {
+            if (!err) {
+                twBox = [];
+                for (var i = 0; i < tweets.length; i++) {
+                    twBox.push({
+                        'created: ' : tweets[i].created_at,
+                        'Tweets: ' : tweets[i].text
+                    });  
+                };
+                console.log(twBox);
+            };
+        })
+    }
+
+    else if (command === 'spotify-this-song') {
+        // Show info about song - Artist, name, preview link, album
+        var Spotify = require('node-spotify-api');
+        var search = function(songName) {
+            var spotify = new Spotify(box.spotifyKeys);
+
+            var artistName = function(artist) {
+                return artist.name;
+            };
+            var songName = process.argv[3] || 'The Sign';
+
+            spotify.search({type: 'track', query: songName, limit: 7}, function(err, data) {
+                if (!err) {
+                    var songs = data.tracks.items;
+                    var compilation = [];
+                    for (var i = 0; i < songs.length; i++) {
+                        compilation.push({
+                            'artist: ' : songs[i].artists.map(artistName),
+                            'song: ' : songs[i].name,
+                            'preview: ' : songs[i].preview_url,
+                            'album: ' : songs[i].album.name,
+                        });
+                    };
+                    console.log(compilation);
+                }
+                else if (err) {
+                    console.log('Error: ' + err)
+                }
+                // else if (songName == undefined) {
+                //     songName = 'The Sign';
+                //     search('The Sign');
+                // };
+            });
+        };
+
+        var select = function(caseData, functionData) {
+            switch (caseData) {
+                case 'spotify-this-song':
+                search();
+                break;
+            default:
+                console.log('申し訳ありませんが、入力が分かりません');
+            }
+        }
+        var runSpotify = function(a, b) {
+            select(a, b);
+        };
+        runSpotify(process.argv[2], process.argv[3]);
+
+    }
+
+    else if (command === 'movie-this') {
+        // Movie - title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
+        var title = qry || 'Mr. Nobody';
+        var queryURL = "http://www.omdbapi.com/?t=" + 
+        title + "&y=&plot=short&apikey=40e9cece";
+        
+            request(queryURL ,function(err, response, body) {
+
+                var film = JSON.parse(body)
+                console.log('Title: ' + film['Title']);
+                console.log('Year: ' + film['Year']);
+                console.log('imdbRating: ' + film['Ratings'][0]['Value']);
+                console.log('Rotten Tomatoes Rating: ' + film['Ratings'][1]['Value']);
+                console.log('Country: ' + film['Country']);
+                console.log('Language: ' + film['Language']);
+                console.log('Plot: ' + film['Plot']);
+                console.log('Actors: ' + film['Actors']);
+
+        });
+
+
+
+    }
+
+    else if (command === 'do-what-it-says') {
+        // Look inside of random.txt
+        var doIt = './random.txt';
+        filez.readFile(doIt, 'utf8', function(err, data) {
+            if (err) throw err;
+            var split = data.split(',');
+            process.argv[2] = split[0];
+            process.argv[3] = split[1];
+            liri(process.argv[2], process.argv[3])
+        });
+    }
+    else {
+        console.log('申し訳ありませんが、入力された命令が分かりません。');
+        console.log('---');
+        console.log('my-tweets');
+        console.log('---');
+        console.log('spotify-this-song');
+        console.log('---');
+        console.log('movie-this');
+        console.log('---');
+        console.log('あるいは、');
+        console.log('---');
+        console.log('do-what-it-says');
+        console.log('---');
+        console.log('を入力してください。')
+    }
+};
+
+liri();
